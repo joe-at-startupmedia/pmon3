51c84ee4f44145dfe6f733d8ff6056fdba8c7c4a
joe-at-startupmedia <joseph.persie@startupmedia.com>
2024-01-09 22:23:34 +0000

    ProcStat debugging and IsRunning code reuse

--- pmond/process/proc_stat.go
+++ pmond/process/proc_stat.go
@@ -1,8 +1,7 @@
 package process
 
 import (
-	"fmt"
-	"os"
+	"pmon3/pmond"
 	"pmon3/pmond/model"
 	"time"
 )
@@ -12,6 +11,12 @@ type ProcStat struct {
 	Process *model.Process
 }
 
+// NewProcStat kicks off two goroutines:
+// the first routine calls os.Process.Wait() which returns when the process exits
+// When the process exits it will return the status code
+// if the first routine doesn't finish (process did not exit) in 200ms
+// the next goroutine will return the result of Process.IsRunning
+// which utilizes proc/%d/stat os.Stat and !os.IsNotExist
 func NewProcStat(p *model.Process) *ProcStat {
 	stat := &ProcStat{
 		Done:    make(chan int),
@@ -38,37 +43,42 @@ func (r *ProcStat) Wait() *model.Process {
 
 func (r *ProcStat) run() {
 	go r.processWait(r.Process)
-	go r.processExistCheck(int(r.Process.Pid))
+	go r.processExistCheck(r.Process.Pid)
 }
 
 func (r *ProcStat) processWait(process *model.Process) {
 	processState, err := process.Pointer.Wait()
 	if err != nil {
+		pmond.Log.Debugf("ProcStat processWait error: %v", err)
 		r.Done <- 1
 		return
 	}
-
+	pmond.Log.Debugf("ProcStat processWait: %v", processState)
 	if processState.Exited() {
 		r.Done <- processState.ExitCode()
 	}
 }
 
-func (r *ProcStat) processExistCheck(pid int) {
+func (r *ProcStat) processExistCheck(pid uint32) {
 	timer := time.NewTicker(time.Millisecond * 200)
 	defer timer.Stop()
 	for {
 		select {
 		case existCode := <-r.Done:
+			pmond.Log.Debugf("ProcStat exitCode: %d", existCode)
 			if existCode != 0 { // process exist exception
 				r.Done <- 1
 				return
 			}
 		case <-timer.C: // check process status by proc file
-			_, err := os.Stat(fmt.Sprintf("/proc/%d/status", pid))
-			if !os.IsNotExist(err) {
+			isRunning := IsRunning(pid)
+			pmond.Log.Debugf("ProcStat timer.C IsRunning: %t", isRunning)
+			if isRunning {
 				r.Done <- 0
-				return
+			} else {
+				r.Done <- 1
 			}
+			return
 		}
 	}
 }
