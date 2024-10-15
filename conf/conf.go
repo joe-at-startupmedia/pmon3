3b645719127bb0f44cba562c952a27f6c6c3f009
joe-at-startupmedia <joseph.persie@startupmedia.com>
2024-10-15 23:02:36 +0000

    1.18.2

--- conf/conf.go
+++ conf/conf.go
@@ -12,7 +12,7 @@ import (
 	"github.com/jinzhu/configor"
 )
 
-const Version string = "1.18.1"
+const Version string = "1.18.2"
 
 type Config struct {
 	Logger                 *logrus.Logger       `yaml:"-"`
